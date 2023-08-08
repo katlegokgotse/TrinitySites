bd3eec05f797e61a7c9b0e7d0efa758359f63ab1
Katlego Kgotse <katlegokgotse88@gmail.com>
2023-08-08 10:48:31 +0200

    Details

---
+++ getInvolved.js
@@ -0,0 +1,6 @@
+let subscribe; 
+function openWebsite(){
+  window.location.href="https://forms.gle/QGhD2RpB5vDb7Ea5A"
+  subscribe = window.open("https://forms.gle/QGhD2RpB5vDb7Ea5A", "_blank", "width=400, height=300, top=230, left=540")
+}
+
